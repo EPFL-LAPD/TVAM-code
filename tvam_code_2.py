5002b568e88aba9413838cda088a4c17943a79c2
dataricerunner <gordonsu@mit.edu>
2024-06-21 11:04:37 +0200

    updated version of tvam_code.py that integrate io triggering from stage to stage to dmd

---
+++ tvam_code_2.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 19 15:21:28 2024
+
+@author: adminlapd
+"""
+
+from zaber_motion.ascii import Connection
+from zaber_motion import Units
+from zaber_motion.ascii import DigitalOutputAction
+import time
+from tqdm import tqdm
+
+
+import os
+import argparse
+
+
+def process_arguments():
+    # Instantiate the parser
+    parser = argparse.ArgumentParser(description='Optional app description')
+    parser.add_argument('-s', '--step_angle', help="step angle in degree. 360/number_of_images, default is 0.36",
+                        action="store", type=float, default=0.36)
+    
+    parser.add_argument('-v', '--velocity', help="rotation speed in deg/sec, default is 40.0",
+                        action="store", type=float, default=40.0)
+    
+    parser.add_argument('-d', '--DMD_duty_cycle', help="DMD duty cycle, default is 0.99",
+                        action="store", type=float, default=0.99)
+    
+    parser.add_argument('-n', '--num_turns', help="number of turns, default is 3",
+                        action="store", type=int, default=3)
+    
+    parser.add_argument('-p', '--path', help="path to images, no default",
+                        action="store")
+    
+    parser.add_argument('-ps', '--port_stage', help="port of the stage",
+                        action="store", type=str, default="COM3")
+    
+    #parser.parse_args(['-h'])
+    args = parser.parse_args()
+    assert args.DMD_duty_cycle < 1 and DMD_duty_cycle > 0.28
+    assert args.velocity <= 80
+    
+    return args
+
+    
+
+def stop_dmd_stage(axis, dmd):
+    try:
+        #dmd.haltProj()
+        #dmd.free()
+        1+1
+    except:
+        pass
+    
+    axis.stop()
+    axis.home()
+    return
+
+
+def initialize_stage(printing_parameters, triggers_per_round=1000):
+    stage_one_turn_steps = 384_000
+
+    assert stage_one_turn_steps % (2 * triggers_per_round) ("{} has to be divisible by 2 * {}".format(stage_one_turn_steps, triggers_per_round))
+
+    stage_handler = Connection.open_serial_port(printing_parameters.port_stage)
+    print("Initialize stage, initialize triggers and reset triggers to 0")
+
+
+    # indicates the steps to turn one round on the stage
+    #Trigger 1 - Set digital output 1 == 1 when pos > 360°
+    stage_handler.generic_command("system restore")
+    
+    # trigger when position >= 360°
+    stage_handler.generic_command("trigger 1 when 1 pos >= {}").format(stage_one_turn_steps) 
+    #set digital output 1 to 1
+    stage_handler.generic_command('trigger 1 action a io set do 1 1')
+    stage_handler.generic_command("trigger 1 enable")
+    
+    
+    stage_handler.generic_command("trigger 3 when 1 pos < {}").format(stage_one_turn_steps)
+    # when it is below <360°, set trigger hard to 0
+    stage_handler.generic_command('trigger 3 action a io set do 1 0') 
+    stage_handler.generic_command("trigger 3 enable")
+    
+    
+    #Trigger 2 - toggle digital output 2 based on distance interval
+    stage_handler.generic_command("trigger 2 when 1 dist {}".format(stage_one_turn_steps // (2 * triggers_per_round)))
+    stage_handler.generic_command("trigger 2 action a io set do 2 t")
+    stage_handler.generic_command("trigger 2 enable")
+    
+    device_list = stage_handler.detect_devices()
+    device = device_list[0]
+    axis = device.get_axis(1)
+    axis.home()
+    return axis
+    
+    
+def print_TVAM(axis, dmd, printing_parameters):
+    axis.move_velocity(printing_parameters.velocity, unit=Units.ANGULAR_VELOCITY_DEGREES_PER_SECOND)
+    
+    
+    try:
+        position = axis.get_position(unit=Units.ANGLE_DEGREES)
+        with tqdm.tqdm(total = 360, desc = "Acceleratinggggg, print after 360°", bar_format= '{l_bar}{bar}{r_bar}') as pbar:
+            while position < 360:
+                position = axis.get_position(unit=Units.ANGLE_DEGREES)
+                pbar.update(int(position)-pbar.n)
+                
+                
+        # DMD.dmd.startContProj(name_of_sequences[0])
+        with tqdm.tqdm(total = 360 * printing_parameters.num_turns, desc = "Printinggggg", bar_format= '{l_bar}{bar}{r_bar}') as pbar:
+            while position < 360 + 360 * printing_parameters.num_turns:
+                position = axis.get_position(unit=Units.ANGLE_DEGREES)
+                pbar.update(int(position)-pbar.n - 360)
+                
+        stop_dmd_stage(axis, dmd)
+
+    except KeyboardInterrupt:
+        print("Keyboard interrupt. Homing stage and stop.")
+        stop_dmd_stage(axis, dmd)
+    print("Print over, inspect sample")
+    
+    return
+
+
+def initialize_DMD():
+    #BASEDIR = r"D:/"
+    #SINOGRAM_DIR = args.path 
+    #IMAGE_DIRECTORY = os.path.join(SINOGRAM_DIR, '*.png')#
+
+    return 0
+
+
+printing_parameters = process_arguments()
+dmd, num_of_images = initialize_DMD()
+axis_stage = initialize_stage(printing_parameters, triggers_per_round=num_of_images)
+print_TVAM(axis_stage, dmd, printing_parameters)
