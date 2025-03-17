e5d040ac0f0287523b208a104fa56e5466868094
epfllapd <fxw+epfllapd@mailbox.org>
2025-03-17 16:24:17 +0100

    Update README

--- generate_numbers_for_DMD.py
+++ generate_numbers_for_DMD.py
@@ -12,13 +12,45 @@ import numpy as np
 import tqdm
 from PIL import ImageFont
 
+img = np.zeros((768, 1024))
 
-for i in tqdm.tqdm(range(1000)):
+on = False
+for i in range(768 // 4):
+    for j in range(1024):
+        img[i, j] = 255 * on 
+        on = not on
+    on = not on
+
+on = False
+factor = 2
+
+img = np.zeros((768, 1024))
+for i in range(768 // factor):
+    for j in range(1024 // factor):
+        img[factor*i:factor*i+(factor), factor*j:factor*j+(factor)] = 255 * on 
+        on = not on
+    on = not on
+
+img = Image.fromarray(img).convert("L")
+img.save("checkerboard_{}x{}.png".format(factor, factor))
+
+
+
+
+exit()
+for i in tqdm.tqdm(range(1)):
     img = np.zeros((768, 1024)) + 255
     
     img[768 // 2 -1 :768 // 2 + 2 , :] = 0
     img[768 // 2 -1 -20:768 // 2 + 2 -20, :] = 0
     img[768 // 2 -1 +20:768 // 2 + 2 +20, :] = 0
+
+    img[768 // 2 -1 -50:768 // 2 + 2 -50, :] = 0
+    img[768 // 2 -1 +50:768 // 2 + 2 +50, :] = 0
+
+    img[768 // 2 -1 -100:768 // 2 + 2 -100, :] = 0
+    img[768 // 2 -1 +100:768 // 2 + 2 +100, :] = 0
+
     # Open an Image 
     img = Image.fromarray(img).convert("L")
      
@@ -39,4 +71,9 @@ for i in tqdm.tqdm(range(1000)):
     #img = np.invert(img)
     #img = Image.fromarray(img).convert("L")
     # Save the edited image
-    img.save("images_line/{:04}.png".format(i))
+    img = np.zeros((768, 1024))
+    #img[100:-100, 100:-100] = 255
+    img[120:648, 200:800] = 255
+    img = Image.fromarray(img).convert("L")
+    img.save("croped_120_648_200_800.png")
+    #img.save("images_white/{:04}.png".format(i))
